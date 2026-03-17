022c36f8883a4b3029bf6487dc3239a3696ef842
Vexas5266 <wd.grove2@gmail.com>
2026-03-17 11:21:27 -0500

    Add FlowControl

---
+++ include/flowControl.hpp
@@ -0,0 +1,71 @@
+#ifndef FLOWCONTROL_H
+#define FLOWCONTROL_H
+
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <iostream>
+#include "grbl.hpp"
+#include "tapeLookup.hpp"
+#include "board.hpp"
+
+#define SPROCKT_R 10 //mm
+#define FEEDER_A 'B'
+#define HEAD_A 'A'
+#define Z_TRAVEL 10
+#define PNP_SPEED 1000
+
+enum class FlowControlState {
+    IDLE,
+    RELOAD,
+    PICK,
+    STOP,
+    COUNT
+};
+
+enum class Places {
+    ORIGIN,
+    FEEEDER,
+    INSPECT,
+    CNT
+};
+
+const coords_t places[(int)Places::CNT] = {
+    { 0, 0,   0,   0 }, /* Origin */
+    { 50, 20,   0,   0 },     /* Feeder    */
+    {  100,  70,   0,   0 },     /* Inspect   */
+};
+
+class FlowControl {
+    private:
+        unique_ptr<Components> m_components;
+
+        FlowControlState m_current_state = FlowControlState::STOP;
+        FlowControlState m_previous_state;
+        uint8_t m_time = 0;
+
+    public:
+
+        FlowControl(const char* commPort);
+
+        ~FlowControl() {
+            cout << "FlowControl DeInit..." << endl;
+            grbl.comm.closeComm();
+        }
+
+        FlowControlState advanceComponent();
+        void setState(FlowControlState state);
+        FlowControlState getState();
+        FlowControlState getPreviousState();
+
+        void tickStateMachine();
+
+        void updateComponents(const char* posFile);
+
+        GRBL grbl;
+
+};
+
+#endif /* FLOWCONTROL_H */
---
+++ src/flowControl.cpp
@@ -0,0 +1,89 @@
+
+#include "flowControl.hpp"
+#include "math.h"
+#include <thread>
+
+#define ARM_FlowControl_FILE "../board/ArmBoard_Hardware-all-pos.csv"
+#define CORE_FlowControl_FILE "../board/CoreBoard-all-pos.csv"
+
+FlowControl::FlowControl(const char* commPort) {
+
+    cout << "Init FlowControl..." << endl;
+
+    #if (INIT_COMM)
+        //Start comm, fill csv
+        cout << "Init Comm..." << endl;
+        if (grbl.comm.setupComm(commPort) == false) {
+            cout << "COM SETUP FAILED" << endl;
+            return;
+        }
+    #else
+        return;
+    #endif
+
+    //Flush startup
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    grbl.comm.readLine(); //Flush return
+    cout << "GRBL Startup:  ";
+    grbl.comm.readLine(); //Startup
+
+    //Init GRBL
+    cout << "GRBL Initializing..." << endl;
+    grbl.comm.writeLine("?");
+    cout << "Startup Status: ";
+    grbl.comm.readLine(); //Status
+    grbl.comm.readLine(); //Flush ok
+
+    //Send GRBL setup commands
+    cout << "Sending GRBL setup commands..." << endl;
+    //TODO: Add setup commands (homing, feed, units, etc.)
+
+    cout << "FlowControl Init Complete." << endl;
+
+    return;
+}
+
+void FlowControl::tickStateMachine()
+{
+    switch (m_current_state)
+    {
+        // Main state machine
+    }
+}
+
+FlowControlState FlowControl::advanceComponent()
+{
+    FlowControlState next_state = FlowControlState::IDLE;
+    components_status_t status = m_components->incrementCurrentComponent();
+
+    if (status == SAME_CUTTAPE) {
+        //Feed next component
+        //Tell Feeder to step forward
+        next_state = FlowControlState::PICK;
+    }
+    else if (status == CHANGE_CUTTAPE) next_state = FlowControlState::RELOAD;
+    else if (status == FINAL_CUTTAPE) next_state = FlowControlState::IDLE;
+
+    return next_state;
+}
+
+void FlowControl::setState(FlowControlState state)
+{
+    m_previous_state = m_current_state;
+    m_current_state = state;
+}
+
+FlowControlState FlowControl::getState()
+{
+    return m_current_state;
+}
+
+FlowControlState FlowControl::getPreviousState()
+{
+    return m_previous_state;
+}
+
+void FlowControl::updateComponents(const char* posFile)
+{
+    m_components = make_unique<Components>(posFile);
+}
