e6476b7c69cd9e9b2617b8492daffc50e6d65180
clayjay3 <claytonraycowen@gmail.com>
2026-05-06 15:52:04 -0500

    Fix get pos

--- include/board.hpp
+++ include/board.hpp
@@ -1,82 +1,102 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-#include <string>
-#include <sstream>
 #include <fstream>
-#include <vector>
-#include <map>
 #include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <vector>
 
 #include "tapeLookup.hpp"
-
-#define P_EXP 0 //Board Fiducial
-#define Q_MEAS 1 //Manual Fiducial
-
-struct component_t {
-    std::string ref;
-    std::string value;
-    std::string package;
-    float posX;
-    float posY;
-    float rotation;
-    std::string side;
-};
-
-struct board_coords_t {
-    double x;
-    double y;
-    double r;
-};
 
 #define DEG2RAD(x) (x * M_PI) / 180.0
 #define RAD2DEG(x) (180.0 * x) / M_PI
 
-enum components_status_t {
-    SAME_CUTTAPE,
-    CHANGE_CUTTAPE,
-    FINAL_CUTTAPE
+struct board_coords_t
+{
+        double x;
+        double y;
+        double r;
 };
 
-class Components {
-
-    private:
-        std::map<std::tuple<std::string, std::string>, std::vector<component_t>> m_notInLookup;
-        std::map<std::tuple<std::string, cuttape_t>, std::vector<component_t>> m_placement_map; // <[Package, Cuttape[P W O]], <Vector of all comp with that package and cuttape>>
-
-        std::map<std::tuple<std::string, cuttape_t>, std::vector<component_t>>::iterator m_cuttape_it;
-        std::vector<component_t>::iterator m_component_it;
-
-        std::vector<board_coords_t> m_fiducials;
-        board_coords_t m_board_offset = {0, 0, 0}; // {mm, mm, radians}
-
-        int m_placed_components = 0;
-
+class Components
+{
     public:
-        Components(const char* csvFile);
+        enum components_status_t
+        {
+            SAME_CUTTAPE,
+            CHANGE_CUTTAPE,
+            FINAL_CUTTAPE
+        };
+
+        enum map_sort_t
+        {
+            VAL,
+            PKG,
+            CTP
+        };
+
+        struct component_t
+        {
+                std::string ref     = "";
+                std::string value   = "";
+                std::string package = "";
+                float posX          = 0.0;
+                float posY          = 0.0;
+                float rotation      = 0.0;
+                std::string side    = "";
+        };
+
+        // <[Value, Package, Cuttape[P W O]], <Vector of all comp with that package and cuttape>>
+        typedef std::map<std::tuple<std::string, std::string, cuttape_t>, std::vector<component_t>> placement_map_t;
+        typedef std::map<std::tuple<std::string, std::string>, std::vector<component_t>> lost_cuttape_map_t;
+
+        ~Components() { std::cout << "Components destroyed" << std::endl; }
 
         void addComponentLookUp(component_t component);
-        void fillLostCuttapes();
-        void parseCSV(std::ifstream& filestream);
+        void updateCuttapeInPlacementMap(std::string value, std::string package, cuttape_t old_cuttape, cuttape_t user_cuttape);
+        void addLostCuttapeToPlacementMap(std::string value, std::string package, cuttape_t user_cuttape);
+        void removeFromPlacementMapToLostMap(std::string value, std::string package, cuttape_t user_cuttape);
+
+        void parseCSV(const char* csvFile);
 
         void printComponents();
 
-        cuttape_t getCurrentCutTape() { return std::get<1>(m_cuttape_it->first); }
+        cuttape_t getCurrentCutTape() { return std::get<CTP>(m_cuttape_it->first); }
+
         component_t getCurrentComponent() { return *m_component_it; }
-        std::map<std::tuple<std::string, cuttape_t>, std::vector<component_t>> getPlacementMap() { return m_placement_map; }
+
+        placement_map_t getPlacementMap() { return m_placement_map; }
+
         int getPlacedComponents() { return m_placed_components; }
 
         std::vector<board_coords_t> getBoardFiducials() { return m_fiducials; }
+
+        lost_cuttape_map_t getLostCuttapes() { return m_lost_cuttape_map; }
+
         void calculateBoardOffset(std::vector<board_coords_t> global_fiducials);
         void transformToPCBCoords(board_coords_t* global_coords);
         void transformToGlobalCoords(board_coords_t* PCB_coords);
-        
+
         void printCoords(board_coords_t coords);
 
-        // After incrementing, returns if it had to move to a new cuttape, 
+        // After incrementing, returns if it had to move to a new cuttape,
         // is in the same cuttape, or is at the end of the map
         components_status_t incrementCurrentComponent();
 
+    private:
+        const char* m_csv_file = "";
+        placement_map_t m_placement_map;
+        lost_cuttape_map_t m_lost_cuttape_map;
+
+        placement_map_t::iterator m_cuttape_it;
+        std::vector<component_t>::iterator m_component_it;
+
+        std::vector<board_coords_t> m_fiducials;
+        board_coords_t m_board_offset = {0, 0, 0};    // {mm, mm, radians}
+
+        int m_placed_components       = 0;
 };
 
 #endif /* BOARD_H */
--- src/LED.cpp
+++ src/LED.cpp
@@ -1,5 +1,20 @@
 #include "LED.hpp"
 
+std::tuple<uint8_t, uint8_t, uint8_t> LED::colorToRGB(LED_COLOR color)
+{
+    switch (color)
+    {
+        case LED_COLOR::RED: return {255, 0, 0};
+        case LED_COLOR::ORANGE: return {255, 165, 0};
+        case LED_COLOR::YELLOW: return {255, 255, 0};
+        case LED_COLOR::GREEN: return {0, 255, 0};
+        case LED_COLOR::BLUE: return {0, 0, 255};
+        case LED_COLOR::PURPLE: return {128, 0, 128};
+        case LED_COLOR::WHITE: return {255, 255, 255};
+        default: return {255, 255, 255};    // Default to white if unknown
+    }
+}
+
 // Setters
 void LED::setBrightness(float brightness)
 {
@@ -9,20 +24,25 @@ void LED::setBrightness(float brightness)
 
 void LED::setOn()
 {
-    m_state = LED_STATE::ON;
-    // m_grbl->sendCommand();
+    m_state        = LED_STATE::ON;
+    auto rgb_color = colorToRGB(m_color);
+    std::cout << "LED Color RGB before brightness: " << std::get<0>(rgb_color) << " " << std::get<1>(rgb_color) << " " << std::get<2>(rgb_color);
+    std::get<0>(rgb_color) = std::get<0>(rgb_color) * (float) m_brightness;
+    std::get<1>(rgb_color) = std::get<1>(rgb_color) * (float) m_brightness;
+    std::get<2>(rgb_color) = std::get<2>(rgb_color) * (float) m_brightness;
+    std::string cmd        = "M69 P" + std::to_string(get<0>(rgb_color)) + " R" + std::to_string(get<1>(rgb_color)) + " S" + std::to_string(get<2>(rgb_color));
+    m_grbl->sendCommand(cmd);
 }
 
 void LED::setOff()
 {
     m_state = LED_STATE::OFF;
-    // m_grbl->sendCommand();
+    m_grbl->sendCommand("M69 P0 R0 S0");    // Command to turn off the first led, need to update to support two leds
 }
 
 void LED::setColor(LED_COLOR color)
 {
     m_color = color;
-    // m_grbl->sendCommand();
 }
 
 // Getters
